629e44ca86a69087b8bf6a9006c3d1aea4779de7
Hossam Khalil <hossam.khalil@espace.com.eg>
2020-06-22 08:57:37 +0200

    React: Section3-43

--- src/App.js
+++ src/App.js
@@ -10,11 +10,25 @@ class App extends Component {
       {name:'ahmed' ,age:'39'}
     ]
   }
+
+  changeNameHandler = () => {
+    // console.log("was clicked");
+    // Dont Do This this.state.persons[0].name = "Hossam Khalil";
+    this.setState({
+      persons: [
+        {name:'hossam Khalil' ,age:'28'},
+        {name:'sherif' ,age:'36'},
+        {name:'ahmed' ,age:'38'}
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, Iam react app</h1>
         <p>This is really working!</p>
+        <button onClick={this.changeNameHandler}>Submit</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Engineering</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
